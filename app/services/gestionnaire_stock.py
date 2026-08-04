6081cd7e86a6a961cac02c4aae6ae0109ef6f9ac
aureliaanani8-glitch <aureliaanani8@gmail.com>
2026-08-04 19:53:07 +0000

    feat: add enregistrer_mouvement method with stock validation

--- app/services/gestionnaire_stock.py
+++ app/services/gestionnaire_stock.py
@@ -1,8 +1,18 @@
 from app.models.produit import Produit
+from app.models.mouvement_stock import MouvementStock, TypeMouvement
 
 class GestionnaireStock:
     def __init__(self):
         self.produits = []
-        
+        self.mouvements = []
     def ajouter_produit(self, produit: Produit):
         self.produits.append(produit)
+        
+    def enregistrer_mouvement(self, mouvement:MouvementStock):
+        if mouvement.type_mouvement == TypeMouvement.SORTIE:
+            if mouvement.quantite > mouvement.produit.quantite:
+                raise ValueError(f"Quantité insuffisante pour le produit {mouvement.produit.nom}: {mouvement.produit.quantite} disponible, {mouvement.quantite} demandé.")
+            mouvement.produit.quantite -= mouvement.quantite
+        else:
+            mouvement.produit.quantite += mouvement.quantite
+        self.mouvements.append(mouvement)
